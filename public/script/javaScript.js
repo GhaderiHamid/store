44e0f9ec7444cc77130b324f30a37fedf3384d3f
Hamid <ghaderihamid86@gmail.com>
2025-03-21 22:17:23 +0330

    frontend view implemented

---
+++ public/script/javaScript.js
@@ -0,0 +1,104 @@
+// //set the date were counting down to
+// var countDownDate=new Date('May 15, 2024 15:37:25').getTime();
+
+// //update the count down every 1 second
+// var x=setInterval(function(){
+//     //Get today's date and time
+//     var now=new Date().getTime();
+
+//     //Find the distance between now and the count down date
+//     var distance=countDownDate-now;
+
+//     //Time calculations for days,hours,minutes and seconds
+//     var days=Math.floor(distance/(1000*60*60*24));
+//     var hours=Math.floor((distance%(1000*60*60*24))/(1000*60*60));
+//     var minutes=Math.floor((distance%(1000*60*60))/(1000*60));
+//     var seconds=Math.floor((distance%(1000*60))/1000);
+
+//     //output the result in a with id="demo"
+//     document.getElementById("demo").innerHTML='<span id="day">'+days+'</span>'
+//     + '<span id="hours">'+hours+'</span>'
+//     + '<span id="minutes">'+minutes+'</span>'
+//     + '<span id="seconds">'+seconds+'</span>';
+
+//     //if the count down is over,write some text
+//     if(distance<0){
+//         clearInterval(x);
+//         document.getElementById("demo").innerHTML=" ";
+//         document.getElementById("offer-expire-text").innerHTML="به پایان رسیده";
+//         document.getElementById("offer-blur").style.filter="blur(2px)";
+//     }
+    
+// },1000);
+// $(document).ready(function(){
+//     // $(".owl-carousel").owlCarousel({
+//     //     rtl:false,
+//     //     items:10,
+//     //     loop:true,
+//     //     margin:10,
+//     //     nav:true,
+//     //     dots:true,
+//     //     autoplay:true,
+//     //     autoplayTimeout:5000,
+//     //     autoplayHoverPause:true,
+//     // });
+//     $('.owl-carousel').owlCarousel({
+//         startPosition: 0,
+//         rtl:true,
+//         loop:true,
+//         margin:10,
+//         items:10,
+//         nav:true,
+//         dots:true,
+//         autoplay:true,
+//         pagination: false,
+//         autoplayTimeout:5000,
+//         autoplayHoverPause:true,
+//         responsive:{
+//             0:{
+//                 items:1
+//             },
+//             600:{
+//                 items:3
+//             },
+//             1000:{
+//                 items:5
+//             }
+//         }
+//     });
+//   });
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel({
+    rtl:true,
+    items:4,
+    loop:true,
+    margin:0,
+    nav:true,
+    slideBy:4,
+    center:true,
+    autoplay:true,
+    autoplayTimeout:3000,
+    autoplayHoverPause:true,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:2,
+            nav:false
+        },
+        1000:{
+            items:3,
+            nav:true,
+            center:false
+        },
+        1200:{
+          items:4,
+          nav:true,
+         
+      }
+    }
+  });
+});
---
+++ resources/views/layouts/frontend/footer.blade.php
@@ -0,0 +1,129 @@
+    <!-- end all products nav -->
+    <!-- start shiping nav -->
+    <div class="container my-5 custom-container">
+        <div class="row justify-content-between no-gutters">
+            <div class="col-sm-12 col-md-6 col-lg-3 col-xl-2">
+                <div class="d-flex align-items-center custom-shipping mt-3">
+                    <img class="img-fluid float-right" src="/img/shipping/shipp- (1).png" alt="" />
+                    <p>ارسال رایگان به تمام نقاط کشور</p>
+                </div>
+            </div>
+            <div class="col-sm-12 col-md-6 col-lg-3 col-xl-2">
+                <div class="d-flex align-items-center custom-shipping mt-3">
+                    <img class="img-fluid float-right" src="/img/shipping/shipp- (2).png" alt="" />
+                    <p>ضمانت اصالت کالا</p>
+                </div>
+            </div>
+            <div class="col-sm-12 col-md-6 col-lg-3 col-xl-2">
+                <div class="d-flex align-items-center custom-shipping mt-3">
+                    <img class="img-fluid float-right" src="/img/shipping/shipp- (3).png" alt="" />
+                    <p>هفت روز ضمانت بازگشت کالا</p>
+                </div>
+            </div>
+            <div class="col-sm-12 col-md-6 col-lg-3 col-xl-2">
+                <div class="d-flex align-items-center custom-shipping mt-3">
+                    <img class="img-fluid float-right" src="/img/shipping/shipp- (4).png" alt="" />
+                    <p>پرداخت آنلاین</p>
+                </div>
+            </div>
+            <div class="col-sm-12 col-md-6 col-lg-3 col-xl-2">
+                <div class="d-flex align-items-center custom-shipping mt-3">
+                    <img class="img-fluid float-right" src="/img/shipping/shipp- (5).png" alt="" />
+                    <p>پشتیبانی 24 ساعته</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- end shiping nav -->
+    <!-- start footer nav -->
+    <footer class="container-fluid custom-footer mt-5">
+        <div class="row">
+            <div class="col-sm-12 col-md-3 text-center">
+                <h5>اطلاعات</h5>
+                <ul class="nav flex-column">
+                    <li class="nav-item">
+                        <a class="nav-link " href="#">درباره ما</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">نحوه خرید</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">محصولات منتخب</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link " href="#">پیشنهادات</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-sm-12 col-md-3 text-center">
+                <h5>خدمات مشتریان</h5>
+                <ul class="nav flex-column">
+                    <li class="nav-item">
+                        <a class="nav-link " href="#">درباره ما</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">نحوه خرید</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">محصولات منتخب</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link " href="#">پیشنهادات</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-sm-12 col-md-3 text-center">
+                <h5>حساب کاربری</h5>
+                <ul class="nav flex-column">
+                    <li class="nav-item">
+                        <a class="nav-link " href="#">درباره ما</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">نحوه خرید</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">محصولات منتخب</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link " href="#">پیشنهادات</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-sm-12 col-md-3 text-center">
+                <h5>اطلاعات تماس</h5>
+                <ul class="nav flex-column">
+                    <li class="nav-item">
+                        <a class="nav-link " href="#">درباره ما</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">نحوه خرید</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">محصولات منتخب</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link " href="#">پیشنهادات</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </footer>
+    <!-- end footer nav -->
+    <?php
+    // $dbc->close();
+    ?>
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.min.js" integrity="sha384-+sLIOodYLS7CIrQpBjl+C7nPvqq+FbNUBDunl/OZv93DB7Ln/533i8e/mZXLi/P+" crossorigin="anonymous">
+    </script>
+    <script src="/script/owl.carousel.min.js"></script>
+    <script src="/script/javaScript.js"></script>
+
+
+
+
+</body>
+
+</html>
