e1babdd63c02bc14c4a0e19d61d449a6359c71e9
hamid <ghaderihamid86@gmail.com>
2025-06-28 16:22:04 +0330

    dbstore implemented

--- resources/views/shipper/index.blade.php
+++ resources/views/shipper/index.blade.php
@@ -9,6 +9,7 @@
 
     <title>پنل مامور ارسال </title>
 
+    
     <!-- فونت‌ها و آیکون‌ها -->
     <link rel="stylesheet"
         href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
