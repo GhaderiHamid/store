e0c1c08dbb86e0ae2ed350c22ec7669e5b28fdab
GhaderiHamid <161806332+GhaderiHamid@users.noreply.github.com>
2025-07-01 17:05:17 +0330

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -20,4 +20,3 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::post('/payment', [PaymentController::class, 'process']);
-});
